ef10678596fda22667620acc30fc7b9ff68cf41e
pauldendulk <pauldendulk@gmail.com>
2016-04-01 16:23:47 +0200

    A rename

--- src/Itinero.Android/TouchHandler.cs
+++ src/Itinero.Android/TouchHandler.cs
@@ -13,7 +13,7 @@ namespace Itinero.Android
 
     class TouchHandler
     {
-        enum TouchState
+        enum TouchMode
         {
             None,
             Dragging,
@@ -24,7 +24,7 @@ namespace Itinero.Android
         public PointF Touch { get; private set; }
         public double Scale { get; private set; }
 
-        TouchState _mode = TouchState.None;
+        TouchMode _mode = TouchMode.None;
         float _oldDist = 1f;
         
         public MapAction Handle(MotionEvent motionEvent)
@@ -39,7 +39,7 @@ namespace Itinero.Android
                 case MotionEventActions.Down:
                     PreviousTouch = new PointF(x, y);
                     Touch = new PointF(x, y);
-                    _mode = TouchState.Dragging;
+                    _mode = TouchMode.Dragging;
                     break;
                 case MotionEventActions.Up:
                     return MapAction.RefreshData;
@@ -49,20 +49,20 @@ namespace Itinero.Android
                     _oldDist = Spacing(motionEvent);
                     MidPoint(Touch, motionEvent);
                     PreviousTouch = Touch;
-                    _mode = TouchState.Zooming;
+                    _mode = TouchMode.Zooming;
                     break;
                 case MotionEventActions.Pointer2Up:
                     return MapAction.RefreshData;
                 case MotionEventActions.Move:
                     switch (_mode)
                     {
-                        case TouchState.Dragging:
+                        case TouchMode.Dragging:
                             Scale = 1;
                             PreviousTouch = Touch;
                             Touch = new PointF(x, y);
                             mapAction = MapAction.RefreshGraphics;
                             break;
-                        case TouchState.Zooming:
+                        case TouchMode.Zooming:
                             if (motionEvent.PointerCount < 2) return MapAction.None;
 
                             var newDist = Spacing(motionEvent);
