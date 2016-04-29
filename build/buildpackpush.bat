1686ad88e8b52788d2b302b695867b84d1409fef
pauldendulk <pauldendulk@gmail.com>
2016-04-29 21:42:03 +0200

    0.1.5

--- build/buildpackpush.bat
+++ build/buildpackpush.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 SETLOCAL
 SET VERSION=%1
-SET NUGET=.\..\tools\nuget\nuget.exe
+SET NUGET=nuget.exe
 
 CALL buildpack %VERSION%
 %NUGET% push .\..\artifacts\Itinero.ui.%VERSION%-alpha.nupkg 
