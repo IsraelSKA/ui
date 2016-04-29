455dafc297b590e033063b6b0ce716248221c89b
pauldendulk <pauldendulk@gmail.com>
2016-04-29 10:52:37 +0200

    Just comments

--- tests/Itinero.Core.Tests/RestrictPanZoomTests.cs
+++ tests/Itinero.Core.Tests/RestrictPanZoomTests.cs
@@ -14,7 +14,7 @@ namespace Itinero.Core.Tests
             var restrictPanZoom = new RestrictPanZoom {PanMode = RestrictPanMode.KeepCenterWithinExtents};
             var viewport = new Viewport { Center = new Point(0, 0), Width = 100, Height = 100, Resolution = 1};
             // viewport.Center is (0, 0) at this point
-            var restrictTo = new BoundingBox(20, 40, 120, 140); // No negative values allowed 
+            var restrictTo = new BoundingBox(20, 40, 120, 140); // Minimal X value is 20, Minimal Y value is 40
 
             // act 
             restrictPanZoom.RestrictPan(viewport, restrictTo);
